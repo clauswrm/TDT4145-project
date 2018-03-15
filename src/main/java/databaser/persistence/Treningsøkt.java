99aaf351b54e0b5993f3fc666e95de4b06492a0c
Ask Sommervoll <askas@stud.ntnu.no>
2018-03-15 17:00:30 +0100

    Completed Treningsøkt, ready for testing of "Notat" and "Treningsøkt"

--- src/main/java/databaser/persistence/Treningsøkt.java
+++ src/main/java/databaser/persistence/Treningsøkt.java
@@ -1,92 +1,101 @@
 package databaser.persistence;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
-public class Treningsøkt {
+public class Treningsøkt extends ActiveDomainObject {
+
+    private int treningsøktID;
+    private Date date;
+    private Time tidspunkt;
+    int varighet;
+    int form;
+    int innsats;
 
 
-/*
-
-        public Treningsøkt(int apparatID, String navn, String beskrivelse) {
-            this.apparatID = apparatID;
-            this.navn = navn;
-            this.beskrivelse = beskrivelse;
-        }
-
-
-        @Override
-        public void save() {
-            if (this.navn == null || this.beskrivelse == null) {
-                throw new IllegalArgumentException("Navn og beskrivelse må være satt");
-            }
-            final String sql = "INSERT INTO apparat (idApparat, Navn, Beskrivelse)" +
-                    "VALUES (?, ?, ?)";
-
-            try (
-                    Connection connection = getConnection();
-                    PreparedStatement statement = connection.prepareStatement(sql);
-            ) {
-
-                setParameters(statement, apparatID, navn, beskrivelse);
-                statement.execute();
-
-            } catch (SQLException e) {
-                throw new RuntimeException("Unable to save apparatus to database.", e);
-            }
-        }
-
-
-
-        @Override
-        public void load() {
-            final String sql = "SELECT * FROM apparat WHERE idApparat=?";
-
-            try (
-                    Connection connection = getConnection();
-                    PreparedStatement statement = connection.prepareStatement(sql);
-            ) {
-
-                setParameters(statement, apparatID);
-                ResultSet resultSet = statement.executeQuery();
-                while (resultSet.next()) {
-                    this.apparatID = resultSet.getInt("idApparat");
-                    this.navn = resultSet.getString("Navn");
-                    this.beskrivelse = resultSet.getString("Beskrivelse");
-                }
-
-            } catch (SQLException e) {
-                throw new RuntimeException("Unable to save apparatus to database");
-            }
-        }
-
-        public static List<databaser.persistence.Apparat> getAll() {
-            final String sql = "SELECT * FROM apparat";
-
-            try (
-                    Connection connection = getConnection();
-                    PreparedStatement statement = connection.prepareStatement(sql);
-            ) {
-
-                ResultSet resultSet = statement.executeQuery();
-                List<databaser.persistence.Apparat> results = new ArrayList<>();
-
-                while (resultSet.next()) {
-                    int apparatID = resultSet.getInt("idApparat");
-                    String navn = resultSet.getString("Navn");
-                    String beskrivelse = resultSet.getString("Beskrivelse");
-
-                    results.add(new databaser.persistence.Apparat(apparatID, navn, beskrivelse));
-                }
-
-                return results;
-
-            } catch (SQLException e) {
-                throw new RuntimeException("Unable to load apparatuses from the database");
-            }
-        } */
+    public Treningsøkt(int treningsøktID, Date date, Time tidspunkt, int varighet, int form, int innsats) {
+        this.treningsøktID = treningsøktID;
+        this.tidspunkt = tidspunkt;
+        this.date = date;
+        this.varighet = varighet;
+        this.form = form;
+        this.innsats = innsats;
     }
+
+
+    @Override
+    public void save() {
+        final String sql = "INSERT INTO treningsøkt (idtreningsøkt, dato, tidspunkt, varighet, form, innsats)" +
+                "VALUES (?, ?, ?, ?, ?, ?)";
+
+        try (
+                Connection connection = getConnection();
+                PreparedStatement statement = connection.prepareStatement(sql);
+        ) {
+
+            setParameters(statement, treningsøktID, date, tidspunkt, varighet, form, innsats);
+            statement.execute();
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Feil: klarte ikke sette inn treningsøkt", e);
+        }
+    }
+
+
+    @Override
+    public void load() {
+        final String sql = "SELECT * FROM treningsøkt WHERE idtreningsøkt=?";
+
+        try (
+                Connection connection = getConnection();
+                PreparedStatement statement = connection.prepareStatement(sql);
+        ) {
+
+            setParameters(statement, treningsøktID);
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                this.treningsøktID = resultSet.getInt("idtreningsøkt");
+                this.date = resultSet.getDate("date");
+                this.tidspunkt = resultSet.getTime("tidspunkt");
+                this.varighet = resultSet.getInt("varighet");
+                this.form = resultSet.getInt("form");
+                this.innsats = resultSet.getInt("innsats");
+
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Feil: klarte ikke lagre treningsøkt");
+        }
+    }
+
+    public static List<Treningsøkt> getAll() {
+        final String sql = "SELECT * FROM treningsøkt";
+
+        try (
+                Connection connection = getConnection();
+                PreparedStatement statement = connection.prepareStatement(sql);
+        ) {
+
+            ResultSet resultSet = statement.executeQuery();
+            List<Treningsøkt> results = new ArrayList<>();
+
+            while (resultSet.next()) {
+                int treningsøktID = resultSet.getInt("idtreningsøkt");
+                Date date = resultSet.getDate("date");
+                Time tidspunkt = resultSet.getTime("tidspunkt");
+                int varighet = resultSet.getInt("varighet");
+                int form = resultSet.getInt("form");
+                int innsats = resultSet.getInt("innsats");
+
+                results.add(new Treningsøkt(treningsøktID, date, tidspunkt, varighet, form, innsats));
+            }
+
+            return results;
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Feil: klarte ikke hente treningsøkter fra databasen");
+        }
+    }
+}
